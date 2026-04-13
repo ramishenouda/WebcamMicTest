16f35765ca1fdef4e26256aa238860e4145c9423
ramishenouda <rami.zaki@limesurvey.org>
2026-04-13 23:34:02 +0200

    Fix JSX MIME type error - add terser and improve Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'automatic',
+  })],
   base: '/',
   publicDir: 'public',
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+  },
 })
